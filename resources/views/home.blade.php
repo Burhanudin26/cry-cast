0fb2c8815ac532114fdd6f1ff51e7ebe0253516e
Alvalen <alvalen.shafel04@gmail.com>
2023-03-14 14:54:15 +0700

    revision fix bug

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
 
   <section class="pt-5 pt-md-0">
           <!-- content -->
+          <div data-bss-parallax-bg="true" class="mx-0" style="background-image: url(pictures/bg.png); background-size: cover; height:100vh; margin: auto 0;">
           <div class="hero min-vh-100 d-flex align-items-center">
             <div class="container">
               <div class="row flex-lg-row-reverse align-items-center">
@@ -22,72 +23,10 @@
               </div>
             </div>
           </div>
+          </div>
   </section>
-  
-  <section class="container-fluid justify-content-center align-items-center vh-100 d-flex flex-column">
-    <div class="container px-0 px-md-5">
-      <div class="row">
-        <div class="col">
-          <h1 class="display-2 fw-bold head-team">Cry Cast</h1>
-        </div>
-      </div>
-      <div class="row">
-        <div class="card">
-          <div class="desc p-4">
-            Cry Cast adalah platform prediksi cryptocurrency yang akurat dengan teknik machine learning canggih seperti
-            Naive Bayes. Kami menggabungkan data real-time dan analisis ahli untuk membantu Anda selangkah lebih maju dan
-            membuat investasi yang menguntungkan. Dengan memanfaatkan kekuatan Naive Bayes, kami mampu mengklasifikasikan
-            dan memprediksi tren pasar dengan tingkat akurasi yang tinggi, sehingga memungkinkan pengguna kami membuat
-            keputusan yang terinformasi. Bergabunglah dengan kami sekarang untuk melihat bagaimana teknologi terbaru kami
-            dapat membantu Anda berhasil di pasar cryptocurrency.
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-  
-  {{-- Page About --}}
-  <section class="vh-100 d-flex flex-column justify-content-center align-items-center margin-team">
-    <div class="container">
-      <div class="row ">
-        <div class="col">
-          <h2 class="head-team text-center">Our Team</h2>
-        </div>
-      </div>
-      <div class="row">
-        {{-- p1 --}}
-        <div class="col-6 col-md-3">
-          <div>
-            <img src="pictures/11.jpg" class="img-fluid rounded-circle shadow" alt="">
-            <h3 class="text-center"> nama </h3>
-          </div>
-        </div>
-        {{-- p2 --}}
-        <div class="col-6 col-md-3">
-          <div>
-            <img src="pictures/11.jpg" class="img-fluid rounded-circle shadow" alt="">
-            <h3 class="text-center"> nama </h3>
-          </div>
-        </div>
-        {{-- p3 --}}
-        <div class="col-6 col-md-3">
-          <div>
-            <img src="pictures/11.jpg" class="img-fluid rounded-circle shadow" alt="">
-            <h3 class="text-center"> nama </h3>
-          </div>
-        </div>
-        {{-- p4 --}}
-        <div class="col-6 col-md-3">
-          <div>
-            <img src="pictures/11.jpg" class="img-fluid rounded-circle shadow" alt="">
-            <h3 class="text-center"> nama </h3>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-  
-    {{-- about --}} 
+
+    {{-- about --}}
     <section class="about pt-3" id="about"
       style="height: 100vh; display: flex;justify-content: center; align-items: center;" >
       <div class="container py-4 py-xl-5">
