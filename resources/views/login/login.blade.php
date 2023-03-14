732f75904d0d805e47e39016870f21f5ad15f7b1
Alvalen <alvalen.shafel04@gmail.com>
2023-03-14 12:44:21 +0700

    add blur to bg

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -4,8 +4,7 @@
     <div class="container py-5 h-100">
       <div class="row d-flex justify-content-center align-items-center h-100">
         <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-          <div class="card text-black" style="border-radius: 1rem;background-color: rgba(255, 255, 255, 0); backdrop-filter: blur(10px);
-">
+          <div class="card text-black" style="border-radius: 1rem;background-color: rgba(255, 255, 255, 0); backdrop-filter: blur(10px);">
             <div class="card-body p-5 text-center">
               <div class="mb-md-5 mt-md-4 pb-5">
                 <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
