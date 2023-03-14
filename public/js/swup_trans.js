69fe5dfad4061c3044ea071b69e75429e38cbc20
Alvalen <alvalen.shafel04@gmail.com>
2023-03-14 16:04:11 +0700

    retrive bug

--- public/js/swup_trans.js
+++ public/js/swup_trans.js
@@ -1,17 +1,1 @@
-// Define SwupScrollPlugin
-class SwupScrollPlugin {
-    name = "SwupScrollPlugin";
-
-    mount() {
-        // Set scroll position to top of page on page change
-        document.documentElement.scrollTop = 0;
-    }
-}
-
-// Initialize Swup with SwupScrollPlugin
-const swup = new Swup({
-    plugins: [new SwupScrollPlugin()],
-});
-swup.on("animationOutDone", function () {
-    window.scrollTo(0, 0);
-});
+const swup = new Swup()
