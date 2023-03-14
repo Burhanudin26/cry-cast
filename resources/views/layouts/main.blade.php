492c45d764c3bab9a9935adb0719b3a82ed781c8
AvanFabian <avanfabiandaniswara@gmail.com>
2023-03-14 20:09:08 +0700

    fix-human-error

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -46,7 +46,7 @@ body::-webkit-scrollbar-thumb {
       <div class="container">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
           aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon">1212</span>
+          <span class="navbar-toggler-icon"></span>
         </button>
         <a class="navbar-brand" href="#">CryCast</a>
         <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
