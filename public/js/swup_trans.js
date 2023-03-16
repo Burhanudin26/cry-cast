19200fd3eba4fa0420202c565593493b01af7376
AvanFabian <avanfabiandaniswara@gmail.com>
2023-03-16 21:48:10 +0700

    Bug-ValidasiTombol-Fixed

--- public/js/swup_trans.js
+++ public/js/swup_trans.js
@@ -19,6 +19,74 @@ swup.on("willReplaceContent", function () {
 });
 
 swup.on("contentReplaced", function () {
+    //nama validation
+	$("#nama").keyup(function () {
+		let nama = $("#nama").val();
+		if (nama.length > 3 && isNaN(nama)) {
+			$("#nama").removeClass("is-invalid");
+			$("#nama").addClass("is-valid");
+			$("#nama").next().next().text("");
+		} else {
+			$("#nama").removeClass("is-valid");
+			$("#nama").addClass("is-invalid");
+			$("#nama")
+				.next()
+				.next()
+				.text("nama must be string and length must be greater than 3");
+		}
+	});
+
+	//email validation
+	$("#email").keyup(function () {
+		let email = $("#email").val();
+		if (email.length > 6 && email.includes("@") && email.includes(".")) {
+			$("#email").removeClass("is-invalid");
+			$("#email").addClass("is-valid");
+			$("#email").next().next().text("");
+		} else {
+			$("#email").removeClass("is-valid");
+			$("#email").addClass("is-invalid");
+			$("#email").next().next().text("email must be valid email");
+		}
+	});
+
+	//password validation must greater than 6 and use regex to check if password contain number and string
+	$("#password").keyup(function () {
+		let password = $("#password").val();
+		let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+		if (password.length > 6 && regex.test(password)) {
+			$("#password").removeClass("is-invalid");
+			$("#password").addClass("is-valid");
+			$("#password").next().next().text("");
+			$("#pashelp").show();
+		} else {
+			$("#password").removeClass("is-valid");
+			$("#password").addClass("is-invalid");
+			$("#password")
+				.next()
+				.next()
+				.text(
+					"password length must be greater than 6 and must contain number, lowercase and uppercase"
+				);
+			$("#rpass").removeClass("is-valid");
+			$("#pashelp").hide();
+		}
+	});
+
+	// confirm password validation
+	$("#rpass").keyup(function () {
+		let rpass = $("#rpass").val();
+		let password = $("#password").val();
+		if (rpass == password) {
+			$("#rpass").removeClass("is-invalid");
+			$("#rpass").addClass("is-valid");
+			$("#rpass").next().next().text("");
+		} else {
+			$("#rpass").removeClass("is-valid");
+			$("#rpass").addClass("is-invalid");
+			$("#rpass").next().next().text("password not match");
+		}
+	});
     // Add event listener to show password button
     $("#showpass").off().on("click", function () {
       if ($("#password").attr("type") === "password") {
@@ -29,6 +97,31 @@ swup.on("contentReplaced", function () {
         $("#password").attr("type", "password");
         $("#rpass").attr("type", "password");
         $("#showpass").text("Show Password");
+      }
+    });
+
+    //enable submit button if all validation is true
+    console.log("nama: " + nama + ", email: " + email + ", password: " + password + ", rpass: " + rpass);
+    $("#submit").addClass("disabled");
+    $("input").keyup(function () {
+      let nama = $("#nama").val();
+      let email = $("#email").val();
+      let password = $("#password").val();
+      let rpass = $("#rpass").val();
+      let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+      if (
+        nama.length > 3 &&
+        isNaN(nama) &&
+        (email.length > 6 &&
+          email.includes("@") &&
+          email.includes(".")) &&
+        password.length > 6 &&
+        regex.test(password) &&
+        rpass == password
+      ) {
+        $("#submit").removeClass("disabled");
+      } else {
+        $("#submit").addClass("disabled");
       }
     });
     
