e2cc3b44067defaf429f86b0fe9d4ff6928175cb
Alvalen <alvalen.shafel04@gmail.com>
2023-03-19 20:39:55 +0700

    back

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,41 +1,40 @@
 @extends('layouts.main')
 @section('content')
   <style>
-    .btn-c {
-      /* bgc gradient blue and purple */
-      background: linear-gradient(45deg, #3a47d5 0%, rgb(0, 162, 255) 100%);
-      color: rgb(255, 255, 255);
-      border: none;
-      border-radius: 10px;
-      padding: 10px 20px;
-      font-size: 20px;
-      font-weight: 600;
-      transition: all ease-in-out 0.5s;
-    }
+.btn-c {
+  /* bgc gradient blue and purple */
+  background: linear-gradient(45deg, #3a47d5 0%, rgb(0, 162, 255) 100%);
+  color: rgb(255, 255, 255);
+  border: none;
+  border-radius: 10px;
+  padding: 10px 20px;
+  font-size: 20px;
+  font-weight: 600;
+  transition: all ease-in-out 0.5s;
+}
 
-    .animated-gradient {
-      background-size: 400% 400%;
-      animation: gradient 5s ease-in-out infinite;
-    }
+.animated-gradient {
+  background-size: 800% 800%;
+  animation: gradient 5s ease-in-out infinite;
+}
 
-    .animated-gradient:hover {
-      animation-play-state: paused;
+.animated-gradient:hover {
+  animation-play-state: paused;
+}
 
-    }
+@keyframes gradient {
+  0% {
+    background-position: 0% 50%;
+  }
 
-    @keyframes gradient {
-      0% {
-        background-position: 0% 50%;
-      }
+  50% {
+    background-position: 100% 50%;
+  }
 
-      50% {
-        background-position: 100% 50%;
-      }
-
-      100% {
-        background-position: 0% 50%;
-      }
-    }
+  100% {
+    background-position: 0% 50%;
+  }
+}
 
     .up {
       text-align: center;
@@ -125,31 +124,7 @@
       </div>
     </div>
   </section>
-  {{-- about --}}
-  <section class="about pt-3" id="about"
-    style="height: 100vh; display: flex;justify-content: center; align-items: center;">
-    <div class="container py-4 py-xl-5">
-      <div class="row gy-4 gy-md-0">
-        <div class="col-md-6" data-aos="slide-up">
-          <div class="p-xl-5 m-xl-5 p-5 m-2 ">
-            <img class="img-fluid" src="{{ asset('pictures/logo/Crycast.png') }}" alt="Alvalen">
-          </div>
-        </div>
-        <div
-          class="col-md-6 text-center text-md-start d-flex d-sm-flex d-md-flex justify-content-center align-items-center justify-content-md-start align-items-md-center justify-content-xl-center">
-          <div style="max-width: 350px;">
-            <h4 class="text-uppercase fw-bold text-dark fontn">About</h4>
-            <p class="my-3 text-dark" style="text-align:justify;">
-              CryCast adalah platform prediksi cryptocurrency yang akurat dengan teknik machine learning canggih seperti
-              Naive Bayes dan Moving Average. Kami menggabungkan data real-time dan analisis ahli untuk membantu Anda
-              membuat keputusan investasi yang menguntungkan. Dengan grafik dan tren yang terupdate, CryCast memungkinkan
-              pengguna untuk memantau dan menganalisis pasar cryptocurrency secara real-time.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+
   {{-- feautures --}}
   <section class="feautures">
     <div class="up mb-5">
@@ -186,6 +161,32 @@
           pengguna membuat keputusan investasi yang terinformasi dengan menampilkan tren historis. Fitur mata uang kripto
           berfokus pada mata uang digital dan potensi dampaknya di masa depan. Terakhir, fitur ramalan bertujuan untuk
           meningkatkan efisiensi investasi dengan memberikan prediksi pasar kepada pengguna.</p>
+      </div>
+    </div>
+  </section>
+
+  {{-- about --}}
+  <section class="about pt-3" id="about"
+    style="height: 100vh; display: flex;justify-content: center; align-items: center;">
+    <div class="container py-4 py-xl-5">
+      <div class="row gy-4 gy-md-0">
+        <div class="col-md-6" data-aos="slide-up">
+          <div class="p-xl-5 m-xl-5 p-5 m-2 ">
+            <img class="img-fluid" src="{{ asset('pictures/logo/Crycast.png') }}" alt="Alvalen">
+          </div>
+        </div>
+        <div
+          class="col-md-6 text-center text-md-start d-flex d-sm-flex d-md-flex justify-content-center align-items-center justify-content-md-start align-items-md-center justify-content-xl-center">
+          <div style="max-width: 350px;">
+            <h4 class="text-uppercase fw-bold text-dark fontn">About</h4>
+            <p class="my-3 text-dark" style="text-align:justify;">
+              CryCast adalah platform prediksi cryptocurrency yang akurat dengan teknik machine learning canggih seperti
+              Naive Bayes dan Moving Average. Kami menggabungkan data real-time dan analisis ahli untuk membantu Anda
+              membuat keputusan investasi yang menguntungkan. Dengan grafik dan tren yang terupdate, CryCast memungkinkan
+              pengguna untuk memantau dan menganalisis pasar cryptocurrency secara real-time.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   </section>
