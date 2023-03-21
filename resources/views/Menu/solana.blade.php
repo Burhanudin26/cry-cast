9a261fb0fb2e0ada728bda3a0170e0a026fe43b2
Alvalen <alvalen.shafel04@gmail.com>
2023-03-21 10:55:20 +0700

    add title

--- resources/views/Menu/solana.blade.php
+++ resources/views/Menu/solana.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.main')
+@section('title', 'Solana')
 @section('content')
 <link rel="stylesheet" href="{{ url('css/menu-cry.css') }}">
   <!-- content -->
--- resources/views/output.blade.php
+++ resources/views/output.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.main')
+@section('title', 'Hasil')
 @section('content')
 <style>
   .tombol-terbang {
