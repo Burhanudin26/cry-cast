e73688f13716a8c43648e28a21f3337c38004ae2
Burhanudin26 <113436431+Burhanudin26@users.noreply.github.com>
2023-03-28 23:11:33 +0700

    v34

--- app/Http/Controllers/NewController.php
+++ app/Http/Controllers/NewController.php
@@ -164,42 +164,42 @@ public function getVolumeData(){
         // $this->getVolumeData();
     }
     //Threshold Naive bayes per bulan
-    public function Threshold()
-    {
-        // Create a PDO connection to the database
-    $db = new PDO('mysql:host=localhost;dbname=crypto', 'root', '');
+//     public function Threshold()
+//     {
+//         // Create a PDO connection to the database
+//     $db = new PDO('mysql:host=localhost;dbname=crypto', 'root', '');
 
-    // Prepare the SQL query to get the low, high, and volume values from the binance table grouped by month
-    $stmt = $db->prepare('SELECT YEAR(date) AS year, MONTH(date) AS month, AVG(low) AS avg_low, AVG(high) AS avg_high, AVG(volume) AS avg_volume FROM binance GROUP BY YEAR(date), MONTH(date)');
+//     // Prepare the SQL query to get the low, high, and volume values from the binance table grouped by month
+//     $stmt = $db->prepare('SELECT YEAR(date) AS year, MONTH(date) AS month, AVG(low) AS avg_low, AVG(high) AS avg_high, AVG(volume) AS avg_volume FROM binance GROUP BY YEAR(date), MONTH(date)');
 
-    // Execute the query
-    $stmt->execute();
+//     // Execute the query
+//     $stmt->execute();
 
-    // Fetch the result as an array of rows
-    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+//     // Fetch the result as an array of rows
+//     $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    // Loop through each row and insert the monthly averages into the MonthlyAverages table
-    foreach ($rows as $row) {
-        $year = $row['year'];
-        $month = $row['month'];
-        $avg_low = $row['avg_low'];
-        $avg_high = $row['avg_high'];
-        $avg_volume = $row['avg_volume'];
+//     // Loop through each row and insert the monthly averages into the MonthlyAverages table
+//     foreach ($rows as $row) {
+//         $year = $row['year'];
+//         $month = $row['month'];
+//         $avg_low = $row['avg_low'];
+//         $avg_high = $row['avg_high'];
+//         $avg_volume = $row['avg_volume'];
 
-        // Prepare the SQL query to insert the average values into the MonthlyAverages table
-        $insert_stmt = $db->prepare('INSERT INTO MonthlyAverages (year, month, avg_low, avg_high, avg_volume) VALUES (:year, :month, :avg_low, :avg_high, :avg_volume)');
+//         // Prepare the SQL query to insert the average values into the MonthlyAverages table
+//         $insert_stmt = $db->prepare('INSERT INTO MonthlyAverages (year, month, avg_low, avg_high, avg_volume) VALUES (:year, :month, :avg_low, :avg_high, :avg_volume)');
 
-        // Bind the average values to the query parameters
-        $insert_stmt->bindParam(':year', $year);
-        $insert_stmt->bindParam(':month', $month);
-        $insert_stmt->bindParam(':avg_low', $avg_low);
-        $insert_stmt->bindParam(':avg_high', $avg_high);
-        $insert_stmt->bindParam(':avg_volume', $avg_volume);
+//         // Bind the average values to the query parameters
+//         $insert_stmt->bindParam(':year', $year);
+//         $insert_stmt->bindParam(':month', $month);
+//         $insert_stmt->bindParam(':avg_low', $avg_low);
+//         $insert_stmt->bindParam(':avg_high', $avg_high);
+//         $insert_stmt->bindParam(':avg_volume', $avg_volume);
 
-        // Execute the query to insert the average values into the MonthlyAverages table
-        $insert_stmt->execute();
-    }
-}
+//         // Execute the query to insert the average values into the MonthlyAverages table
+//         $insert_stmt->execute();
+//     }
+// }
     //Membuat bullish dan bearish pada moving average
     public function BB(){
 
@@ -222,7 +222,7 @@ public function getVolumeData(){
                 'volume' => is_numeric($row[8]) ? $row[8] : 0,
     ]);}}
     $this->AverageAll();
-    $this->Threshold();
+    // $this->Threshold();
 }
 
     //Bitcoin
